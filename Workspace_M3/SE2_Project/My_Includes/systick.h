75aa785b6c2902a06f60b374bfa463904c0e58b8
nba1992 <nunoboa_alma@hotmail.com>
2014-05-05 15:21:51 +0100

    blink_led finished and SE2 Project initialized

---
+++ Workspace_M3/SE2_Project/My_Includes/systick.h
@@ -0,0 +1,15 @@
+#ifndef systick
+#define systick
+
+#include "LPC1769.h"
+
+typedef struct {
+
+LPC1769_Reg STCTRL;
+LPC1769_Reg STRELOAD;
+LPC1769_Reg STCURR;
+LPC1769_Reg STCALIB;
+
+}LPC1769_SYSTICK;
+
+#endif
