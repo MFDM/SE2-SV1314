34aea575dca469fd0b0ad429c78c46b6e33ad65f
MFDiasMarques91 <mfdiasmarques@gmail.com>
2014-05-22 17:31:15 +0100

    SPI init

--- Workspace_M3/SE2_Project/src/spi.c
+++ Workspace_M3/SE2_Project/src/spi.c
@@ -3,6 +3,7 @@
 #include "pcb.h"
 #include "spi.h"
 
+#define spi_freq 400000
 
 #define pcompPins 	(1<<8)
 #define pclksel0_17 (1<<17)
@@ -16,32 +17,59 @@
 #define mosi_05 	(1<<5)
 #define mosi_04		(1<<4)
 
+#define BE 		(1<<2)
+#define CPHA 	(1<<3)
+#define CPOC 	(1<<4)
+#define MSTR	(1<<5)
+#define LSBF	(1<<6)
+#define SPIE	(1<<7)
+#define B8		(1<<8)
+#define B9		(1<<9)
+#define B10 	(1<<10)
+#define B11 	(1<<11)
+
 LPC1769_Reg* ptr_pcnp = LPC1769_PCONP;
 LPC1769_Reg* ptr_pclksel0 = LPC1769_PCLKSEL0;
 LPC1769_PCB* pcb_Regs = LPC1769_BASE_PCB;
+LPC1769_SPI* spiRegs = LPC1769_BASE_SPI;
+
+#define set(mask) (spiRegs->SOSPCR = mask)
+#define clear(mask) (spiRegs->SOSPCR = mask)
 
 void SPI_Init(){
-	*ptr_pcnp &= pcompPins;
-	*ptr_pclksel0 &= ~pclksel0_17;
-	*ptr_pclksel0 &= ~pclksel0_16;
+	*ptr_pcnp |= pcompPins;
+	*ptr_pclksel0 |= ~pclksel0_17;
+	*ptr_pclksel0 |= ~pclksel0_16;
 
 	// SCK reseted
-	pcb_Regs->PINSEL0 &= ~sck_31;
-	pcb_Regs->PINSEL0 &= ~sck_30;
+	pcb_Regs->PINSEL0 |= ~sck_31;
+	pcb_Regs->PINSEL0 |= ~sck_30;
 
 	// SSEL reseted
-	pcb_Regs->PINSEL1 &= ~ssel_01;
-	pcb_Regs->PINSEL1 &= ~ssel_00;
+	pcb_Regs->PINSEL1 |= ~ssel_01;
+	pcb_Regs->PINSEL1 |= ~ssel_00;
 
 	// MISO mode denied
-	pcb_Regs->PINSEL1 &= miso_03;
-	pcb_Regs->PINSEL1 &= miso_02;
+	pcb_Regs->PINSEL1 |= miso_03;
+	pcb_Regs->PINSEL1 |= miso_02;
 
 	// MOSI mode
-	pcb_Regs->PINSEL1 &= ~mosi_05;
-	pcb_Regs->PINSEL1 &= ~mosi_04;
+	pcb_Regs->PINSEL1 |= ~mosi_05;
+	pcb_Regs->PINSEL1 |= ~mosi_04;
 }
 
 int SPI_Tranfer(){
- return 0;
+	// Setting up clock frequency
+	SystemCoreClockUpdate();
+	unsigned div = (SystemCoreClock/4) / spi_freq;
+
+	// Setting up master mode
+	set( BE | CPHA | CPOL | MSTR | (~LSBF) | (~SPIE) | B8 | (~B9) | (~B10) | B11 );
+
+
+
+
+
+
+	return 0;
 }
