64b30b3eb66946104448f220f8d5a34eb92a1e3d
nba1992 <nunoboa_alma@hotmail.com>
2014-05-28 17:31:50 +0100

    commit spi

--- Workspace_M3/SE2_Project/src/spi.c
+++ Workspace_M3/SE2_Project/src/spi.c
@@ -27,6 +27,7 @@
 #define B9		(1<<9)
 #define B10 	(1<<10)
 #define B11 	(1<<11)
+#define SPIF 	(1<<7)
 
 LPC1769_Reg* ptr_pcnp = LPC1769_PCONP;
 LPC1769_Reg* ptr_pclksel0 = LPC1769_PCLKSEL0;
@@ -38,38 +39,50 @@ LPC1769_SPI* spiRegs = LPC1769_BASE_SPI;
 
 void SPI_Init(){
 	*ptr_pcnp |= pcompPins;
-	*ptr_pclksel0 |= ~pclksel0_17;
-	*ptr_pclksel0 |= ~pclksel0_16;
+	*ptr_pclksel0 |= pclksel0_17;
+	*ptr_pclksel0 |= pclksel0_16;
 
 	// SCK reseted
-	pcb_Regs->PINSEL0 |= ~sck_31;
-	pcb_Regs->PINSEL0 |= ~sck_30;
+	pcb_Regs->PINSEL0 |= sck_31;
+	pcb_Regs->PINSEL0 |= sck_30;
 
 	// SSEL reseted
-	pcb_Regs->PINSEL1 |= ~ssel_01;
-	pcb_Regs->PINSEL1 |= ~ssel_00;
+	pcb_Regs->PINSEL1 |= ssel_01;
+	pcb_Regs->PINSEL1 |= ssel_00;
 
 	// MISO mode denied
 	pcb_Regs->PINSEL1 |= miso_03;
 	pcb_Regs->PINSEL1 |= miso_02;
 
 	// MOSI mode
-	pcb_Regs->PINSEL1 |= ~mosi_05;
-	pcb_Regs->PINSEL1 |= ~mosi_04;
+	pcb_Regs->PINSEL1 |= mosi_05;
+	pcb_Regs->PINSEL1 |= mosi_04;
+
+	// SPI0 SCK = : PCLK_SPI / SPCCR0
+			falta faer set do count
+
+	// Setting up master mode
+	set( BE | CPHA | CPOL | MSTR | B8 | B11 );
+	spiRegs->SOSPCR &= (~LSBF);
+	spiRegs->SOSPCR &= (~SPIE);
+	spiRegs->SOSPCR &= (~B9);
+	spiRegs->SOSPCR &= (~B10);
+	unsigned int val = spiRegs->SOSPCR; // for debug
 }
 
-int SPI_Tranfer(){
+int SPI_Tranfer(char data, char DnC){
 	// Setting up clock frequency
 	SystemCoreClockUpdate();
 	unsigned div = (SystemCoreClock/4) / spi_freq;
+	short first_bit = 0x1;
+	DnC &= first_bit;
 
-	// Setting up master mode
-	set( BE | CPHA | CPOL | MSTR | (~LSBF) | (~SPIE) | B8 | (~B9) | (~B10) | B11 );
-
-
-
-
-
+	//transfer
+	spiRegs->SOSPDR =((DnC<<7)|data);
+	while((spiRegs->SOSPSR & SPIF) != SPIF);
+	spiRegs->SOSPDR;// This bit is cleared by first reading this
+	//register, then accessing the SPI Data Register.
 
 	return 0;
 }
+
