824c5dbd7e1b6f4c0c739daedbb02610faaef856
MFDiasMarques91 <mfdiasmarques@gmail.com>
2014-06-05 14:46:34 +0100

    C file & Header file of enet added

--- Workspace_M3/SE2_Project/My_Includes/LPC1769_Types.h
+++ Workspace_M3/SE2_Project/My_Includes/LPC1769_Types.h
@@ -1,6 +1,8 @@
 #ifndef LPC1769_TYPES
 #define LPC1769_TYPES
 
+#include "lpc_types.h"
+
 typedef volatile unsigned int LPC1769_Reg;
 
 #endif
---
+++ Workspace_M3/SE2_Project/src/enet.c
@@ -0,0 +1,84 @@
+#include "enet.h"
+
+STATIC ENET_RXDESC_T *pRXDescs = (ENET_RXDESC_T *) ENET_RX_DESC_BASE;
+STATIC ENET_RXSTAT_T *pRXStats = (ENET_RXSTAT_T *) ENET_RX_STAT_BASE;
+STATIC ENET_TXDESC_T *pTXDescs = (ENET_TXDESC_T *) ENET_TX_DESC_BASE;
+STATIC ENET_TXSTAT_T *pTXStats = (ENET_TXSTAT_T *) ENET_TX_STAT_BASE;
+
+/* Transmit/receive buffers and indices */
+STATIC int32_t rxConsumeIdx;
+STATIC int32_t txProduceIdx;
+
+
+/* Initialize MAC descriptors for simple packet receive/transmit */
+STATIC void InitDescriptors(void)
+{
+	int i;
+
+	/* Setup the descriptor list to a default state */
+	memset(pTXDescs, 0, ENET_NUM_TX_DESC * sizeof(ENET_TXDESC_T));
+	memset(pTXStats, 0, ENET_NUM_TX_DESC * sizeof(ENET_TXSTAT_T));
+	memset(pRXDescs, 0, ENET_NUM_RX_DESC * sizeof(ENET_RXDESC_T));
+	memset(pRXStats, 0, ENET_NUM_RX_DESC * sizeof(ENET_RXSTAT_T));
+
+	rxConsumeIdx = 0;
+    rxConsumeIdx = 0;
+
+	/* Build linked list, CPU is owner of descriptors */
+	for (i = 0; i < ENET_NUM_RX_DESC; i++) {
+		pRXDescs[i].Packet = (uint32_t) ENET_RX_BUF(i);
+		pRXDescs[i].Control = ENET_RCTRL_SIZE(ENET_ETH_MAX_FLEN);
+	}
+	for (i = 0; i < ENET_NUM_TX_DESC; i++) {
+		pTXDescs[i].Packet = (uint32_t) ENET_TX_BUF(i);
+	}
+
+	/* Setup list pointers in Ethernet controller */
+	Chip_ENET_InitTxDescriptors(LPC_ETHERNET, pTXDescs, pTXStats, ENET_NUM_TX_DESC);
+	Chip_ENET_InitRxDescriptors(LPC_ETHERNET, pRXDescs, pRXStats, ENET_NUM_RX_DESC);
+}
+
+/* Get the pointer to the Rx buffer storing new received frame */
+STATIC void *ENET_RXGet(int32_t *bytes)
+{
+	uint16_t produceIdx;
+	void *buffer;
+
+	produceIdx = Chip_ENET_GetRXProduceIndex(LPC_ETHERNET);
+	/* This doesn't check status of the received packet */
+	if (Chip_ENET_GetBufferStatus(LPC_ETHERNET, produceIdx, rxConsumeIdx, ENET_NUM_RX_DESC) != ENET_BUFF_EMPTY) {
+		/* CPU owns descriptor, so a packet was received */
+		buffer = (void *) pRXDescs[rxConsumeIdx].Packet;
+		*bytes = (int32_t) (ENET_RINFO_SIZE(pRXStats[rxConsumeIdx].StatusInfo) - 4);/* Remove CRC */
+	}
+	else {
+		/* Nothing received */
+		*bytes = 0;
+		buffer = NULL;
+	}
+
+	return buffer;
+}
+
+/* Release Rx Buffer */
+STATIC void ENET_RXBuffClaim(void)
+{
+	rxConsumeIdx = Chip_ENET_IncRXConsumeIndex(LPC_ETHERNET);
+}
+
+STATIC int ENET_checkBuffer(){
+	int32_t* bytes;
+	InitDescriptors();
+	void* buffer = ENET_RXGet(bytes);
+
+	if(bytes != 0) return 1;
+
+	ENET_RXBuffClaim();
+
+	return 0;
+}
+
+
+
+
+
