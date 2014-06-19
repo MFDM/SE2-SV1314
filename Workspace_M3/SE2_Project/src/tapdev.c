13cd0845d9dc4eb8827adcf3a5c2a3a4c944b232
MFDiasMarques91 <mfdiasmarques@gmail.com>
2014-06-19 16:22:14 +0100

    tapdev.c done.

--- Workspace_M3/SE2_Project/src/tapdev.c
+++ Workspace_M3/SE2_Project/src/tapdev.c
@@ -60,87 +60,41 @@
 //static int fd;
 //
 //
+
+#include "chip.h"
+#include "lpc_types.h"
+#include "enet_17xx_40xx.h"
+
+#define useRMMI TRUE
+
 ///*---------------------------------------------------------------------------*/
-//void
-//tapdev_init(void)
-//{
-//  char buf[1024];
-//
-//  fd = open(DEVTAP, O_RDWR);
-//  if(fd == -1) {
-//    perror("tapdev: tapdev_init: open");
-//    exit(1);
-//  }
-//
-//#ifdef linux
-//  {
-//    struct ifreq ifr;
-//    memset(&ifr, 0, sizeof(ifr));
-//    ifr.ifr_flags = IFF_TAP|IFF_NO_PI;
-//    if (ioctl(fd, TUNSETIFF, (void *) &ifr) < 0) {
-//      perror(buf);
-//      exit(1);
-//    }
-//  }
-//#endif /* Linux */
-//
-//  snprintf(buf, sizeof(buf), "ifconfig tap0 inet %d.%d.%d.%d",
-//	   UIP_DRIPADDR0, UIP_DRIPADDR1, UIP_DRIPADDR2, UIP_DRIPADDR3);
-//  system(buf);
-//
-//}
+void
+tapdev_init(void){
+	Chip_ENET_Setup(LPC_ETHERNET, useRMMI);
+}
+
 ///*---------------------------------------------------------------------------*/
-//unsigned int
-//tapdev_read(void)
-//{
-//  fd_set fdset;
-//  struct timeval tv, now;
-//  int ret;
-//
-//  tv.tv_sec = 0;
-//  tv.tv_usec = 1000;
-//
-//
-//  FD_ZERO(&fdset);
-//  FD_SET(fd, &fdset);
-//
-//  ret = select(fd + 1, &fdset, NULL, NULL, &tv);
-//  if(ret == 0) {
-//    return 0;
-//  }
-//  ret = read(fd, uip_buf, UIP_BUFSIZE);
-//  if(ret == -1) {
-//    perror("tap_dev: tapdev_read: read");
-//  }
-//
-//  /*  printf("--- tap_dev: tapdev_read: read %d bytes\n", ret);*/
-//  /*  {
-//    int i;
-//    for(i = 0; i < 20; i++) {
-//      printf("%x ", uip_buf[i]);
-//    }
-//    printf("\n");
-//    }*/
-//  /*  check_checksum(uip_buf, ret);*/
-//  return ret;
-//}
+unsigned int
+tapdev_read(void){
+	void* buff = null;
+	int32_t bytes = 0;
+	buff = ENET_RXGet(&bytes);
+	if(bytes){
+		memcpy(uip_buf, buff, bytes);
+		uip_len = bytes;
+	}
+	Chip_ENET_IncRXConsumeIndex(LPC_ETHERNET);
+
+	return bytes;
+}
+
 ///*---------------------------------------------------------------------------*/
-//void
-//tapdev_send(void)
-//{
-//  int ret;
-//  /*  printf("tapdev_send: sending %d bytes\n", size);*/
-//  /*  check_checksum(uip_buf, size);*/
-//
-//  /*  drop++;
-//  if(drop % 8 == 7) {
-//    printf("Dropped a packet!\n");
-//    return;
-//    }*/
-//  ret = write(fd, uip_buf, uip_len);
-//  if(ret == -1) {
-//    perror("tap_dev: tapdev_send: writev");
-//    exit(1);
-//  }
-//}
+void
+tapdev_send(void){
+	void* bufAddr = ENET_TXBuffGet();
+	memcpy(bufAddr, uip_buf, uip_len);
+	ENET_TXQueue(uip_len);
+
+}
+
 ///*---------------------------------------------------------------------------*/
